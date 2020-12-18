aa020c6bfeb68873edfa1094f5feac028b5530c2
neilmarshall <neil.marshall@dunelm.org.uk>
2020-12-18 16:57:39 +0000

    add example of using arguments

--- GraphQLDemo.API/GraphQL/BikeStoreQuery.cs
+++ GraphQLDemo.API/GraphQL/BikeStoreQuery.cs
@@ -1,4 +1,5 @@
-﻿using GraphQL.Types;
+﻿using GraphQL;
+using GraphQL.Types;
 using GraphQLDemo.API.GraphQLModel.Types.Production;
 using GraphQLDemo.Interfaces.Repository;
 
@@ -13,7 +14,15 @@ namespace GraphQLDemo.API.GraphQLModel
                 resolve: context => bikeStoreRepository.GetBrands());
             Field<ListGraphType<ProductType>>(
                 "products",
-                resolve: context => bikeStoreRepository.GetProducts(1, 4));
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<IntGraphType>> { Name="page" },
+                    new QueryArgument<NonNullGraphType<IntGraphType>> { Name="itemsPerPage" }),
+                resolve: context =>
+                {
+                    var page = context.GetArgument<int>("page");
+                    var itemsPerPage = context.GetArgument<int>("itemsPerPage");
+                    return bikeStoreRepository.GetProducts(page, itemsPerPage);
+                });
         }
     }
 }
