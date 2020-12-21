b3a741342eebb542bc57f02e64cbd2e6b65b3bda
neilmarshall <neil.marshall@dunelm.org.uk>
2020-12-21 11:53:14 +0000

    add GraphQL Client (and test fixture)

---
+++ GraphQLDemo.Client.Tests/GraphQLDemoClientFixtures.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GraphQLDemo.Interfaces.Models.Production;
+
+namespace GraphQLDemo.Client.Tests
+{
+    [TestClass]
+    public class GraphQLDemoClientFixtures
+    {
+        [TestMethod]
+        public async Task GetBrandsFixture()
+        {
+            var client = new GraphQLDemoClient("https://localhost:5001/graphql");
+
+            var actual = await client.GetBrands();
+
+            var expected = new Brand[]
+            {
+                new Brand { Id = 1, BrandName = "Electra" },
+                new Brand { Id = 2, BrandName = "Haro" },
+                new Brand { Id = 3, BrandName = "Heller" },
+                new Brand { Id = 4, BrandName = "Pure Cycles" },
+                new Brand { Id = 5, BrandName = "Ritchey" },
+                new Brand { Id = 6, BrandName = "Strider" },
+                new Brand { Id = 7, BrandName = "Sun Bicycles" },
+                new Brand { Id = 8, BrandName = "Surly" },
+                new Brand { Id = 9, BrandName = "Trek" }
+            };
+
+            foreach (var (e, a) in expected.Zip(actual, (e, a) => (e, a)))
+            {
+                Assert.AreEqual(e.Id, a.Id);
+                Assert.AreEqual(e.BrandName, a.BrandName);
+            }
+        }
+    }
+}
---
+++ GraphQLDemo.Client/GraphQLDemoClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Client.Abstractions;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.SystemTextJson;
+using GraphQLDemo.Interfaces.Models.Production;
+
+namespace GraphQLDemo.Client
+{
+    public class GraphQLDemoClient
+    {
+        private GraphQLHttpClient _client;
+
+        public GraphQLDemoClient(string url)
+        {
+            this._client = new GraphQLHttpClient(url, new SystemTextJsonSerializer());
+        }
+
+        public async Task<Brand[]> GetBrands()
+        {
+            var request = new GraphQLRequest
+            {
+                Query = @"query {brands {id brandName}}"
+            };
+
+            var brands = await this._client.SendQueryAsync(request, () => new { Brands = Array.Empty<Brand>() });
+
+            return brands.Data.Brands;
+        }
+    }
+}
